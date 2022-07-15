1357fdec08ea293edba616d509694a5c175f1ad1
Blaise Porret <porretb@gmail.com>
2022-07-15 07:52:00 +0200

    modification fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -3,17 +3,17 @@
     [
         [
             'titre' => 'Pain',
-            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+            'recette' => '20 grammes de farine, 2 dl d\'eau'
         ]
         ,
         [
             'titre' => 'Patisserie',
-            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+            'recette' => '20 grammes de farine, 2 dl d\'eau'
         ]
         ,        
         [
             'titre' => 'Plat',
-            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+            'recette' => '20 grammes de farine, 2 dl d\'eau'
         ]
     ];
 
