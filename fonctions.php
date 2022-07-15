caf84b411f88246027c3fd75673fd9a55fe90922
Blaise Porret <porretb@gmail.com>
2022-07-15 07:36:52 +0200

    pull

---
+++ fonctions.php
@@ -0,0 +1,20 @@
+<?php
+    $recette = 
+    [
+        [
+            'titre' => 'Pain',
+            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+        ]
+        ,
+        [
+            'titre' => 'Patisserie',
+            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+        ]
+        ,        
+        [
+            'titre' => 'Plat',
+            'recette' => ,'20 grammes de farine, 2 dl d\'eau'
+        ]
+    ];
+
+?>
