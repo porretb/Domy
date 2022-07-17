4858138f89f60bca6b53de1be760687df9de34d7
porretb <98593621+porretb@users.noreply.github.com>
2022-07-17 16:48:59 +0200

    ajout du menu en php

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
     <div class="element_menu">
         <h3>Menu du boulanger</h3>
         <ul>
-        <li><a href="page1.html">Page 1</a></li>
-        <li><a href="page2.html">Page 2</a></li>
-        <li><a href="page3.html">Page 3</a></li>
+        <li><a href="page1.php">Page 1</a></li>
+        <li><a href="page2.php">Page 2</a></li>
+        <li><a href="page3.php">Page 3</a></li>
         </ul>
     </div>
 </nav>
