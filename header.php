be24c972d22db056136b1460454ecbd3c309cbaf
porretb <98593621+porretb@users.noreply.github.com>
2022-08-03 21:39:37 +0200

    css

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <nav id="menu">
     <div class="element_menu">
-        <h3>Menu du boulanger</h3>
         <ul>
-        <li><a href="page1.php">Page 1</a></li>
-        <li><a href="page2.php">Page 2</a></li>
-        <li><a href="page3.php">Page 3</a></li>
+        <li><a href="page1.php">Accueil</a></li>
+        <li><a href="page2.php">Les recettes</a></li>
+        <li><a href="page3.php">Contact</a></li>
         </ul>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -4,26 +4,38 @@
         <meta charset="utf-8">
         <title>Domy - recette du boulanger</title>
         <link rel="stylesheet" href="css.scss">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Paytone One">
     </head>
     <body>
-        <?php 
-            include('header.php'); 
-            include('variables.php'); 
-            include('fonction.php'); 
-        ?>
-        <?php foreach(getRecette($recipes) as $recipe) : ?>
-            <article>
-                <h3>
-                    <?php echo $recipe['titre_recette']; ?>
-                </h3>
-                <div>
-                    <?php echo $recipe['ingredients']?>
-                </div>
-                <i>
-                    <?php echo afficheAuteur($recipe['auteur'], $user);?>
-                </i>
-            </article>
-        <?php endforeach ?>
-        <?php include('footer.php'); ?>  
+
+        <header>
+            <h1>Domy</h1>
+            <?php 
+                include('header.php'); 
+                include('variables.php'); 
+                include('fonction.php'); 
+            ?>
+        </header>
+
+        <section id="recette">
+            <?php foreach(getRecette($recipes) as $recipe) : ?>
+                <article>
+                    <h3>
+                        <?php echo $recipe['titre_recette']; ?>
+                    </h3>
+                    <div>
+                        <?php echo $recipe['ingredients']?>
+                    </div>
+                    <i>
+                        <?php echo afficheAuteur($recipe['auteur'], $user);?>
+                    </i>
+                </article>
+            <?php endforeach ?>
+        </section>
+
+        <footer>
+            <?php include('footer.php'); ?>
+        </footer>  
+
     </body>
 </html>
